e91084cb326d02227a8b8a3d60c8af84c530509c
Dylan Sanders <sandersd27@gmail.com>
2015-04-11 20:43:44 +0200

    Added client form

---
+++ client/templates/posts/post_submit.js
@@ -0,0 +1,17 @@
+Template.postSubmit.helpers({
+
+   'submit form': function(e) {
+
+       e.preventDefault();
+
+       var post = {
+
+           url: $(e.target).find('[name=url]').val(),
+           title: $(e.target).find('[name=title]').val()
+       };
+
+       post._id = Posts.insert(post);
+       Router.go('postPage', post);
+
+   }
+});
